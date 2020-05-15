f9fdcb4d6e6dfb1f18d94a3c9e0b20e6c816c2ba
lenovo <lenovo@LAPTOP-P8K9KTT5>
2020-05-15 14:57:24 +0800

    11_3

--- 11_3/11_3.cpp
+++ 11_3/11_3.cpp
@@ -2,10 +2,26 @@
 //
 
 #include "stdafx.h"
-
+#include <opencv2/opencv.hpp>
+using namespace cv;
 
 int main()
 {
-    return 0;
+	cv::Mat src = imread("D:\\gtest.jpg", 0);
+	cv::Mat dst;
+	uchar Lut[256];
+	for (int i = 0; i<256; i++) {
+		float a = (float)i / 255;
+		float b = powf(a, 0.4);
+		Lut[i] = b * 255;
+	}
+	src.copyTo(dst);
+	MatIterator_<uchar> it, end;
+	for (it = dst.begin<uchar>(), end = dst.end<uchar>(); it != end; it++) {
+		*it = Lut[(*it)];
+	}
+	cv::imshow("src", src);
+	cv::imshow("dst", dst);
+	cv::waitKey(0);
+	return 0;
 }
-
